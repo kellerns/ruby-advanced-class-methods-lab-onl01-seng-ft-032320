bc9cb1cfd621dde7c4598bfc532b94c1a0d3224b
Nicholas Keller <nic.keller328@gmail.com>
2020-04-05 21:49:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,88 @@
 class Song
-  attr_accessor :name, :artist_name
+ attr_accessor :name, :artist_name
   @@all = []
 
   def self.all
     @@all
   end
-
+  
   def save
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  
+  def name
+      @name
+  end
+  
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+  
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+  
+  def self.find_by_name(song_name)
+    found_song = ""
+    @@all.each do |song|
+      if song.name == song_name
+        found_song = song
+      else
+      end
+    end
+    if found_song == ""
+      nil
+    else
+      found_song
+    end
+  end
+    
+    
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name) == nil
+      new_song = self.create_by_name(song_name)
+    else
+      self.find_by_name(song_name)
+    end
+  end
+    
+  def self.alphabetical
+    @@all.sort do |a, b|
+    a.name <=> b.name
+    end
+  end
+  
+  def self.new_from_filename(file_name)
+    file_array = file_name.split(/[\-\.]/)
+    file_array.collect do |name|
+      name.rstrip!
+      name.rstrip
+      name.lstrip
+      name.lstrip!
+    end
+    file_array
+    song = self.create_by_name(file_array[1])
+    song.artist_name = file_array[0]
+    song
+  end
+  
+  def self.create_from_filename(file_name)
+    self.new_from_filename(file_name)
+  end
+
+    
+  def self.destroy_all
+    @@all = []
+  end
+  
 end
